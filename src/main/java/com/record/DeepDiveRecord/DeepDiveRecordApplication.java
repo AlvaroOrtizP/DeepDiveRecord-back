5d1be84d870a124e6b697b9a1b7de4c6b2c88d96
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2024-11-27 11:54:05 +0100

    docker

--- src/main/java/com/record/DeepDiveRecord/DeepDiveRecordApplication.java
+++ src/main/java/com/record/DeepDiveRecord/DeepDiveRecordApplication.java
@@ -11,21 +11,43 @@ public class DeepDiveRecordApplication {
 		// Detectar si estamos dentro de Docker o no
 		String dotenvDirectory = System.getenv("DOTENV_DIR") != null ? System.getenv("DOTENV_DIR") : ".";
 
-		// Cargar las variables desde el archivo .env en la ruta correcta
+		// Cargar las variables desde el archivo .env en la ruta correcta (solo en local)
 		Dotenv dotenv = Dotenv.configure()
 				.directory(dotenvDirectory) // Usa la ruta correspondiente
 				.load();
 
-		// Configurar variables de entorno manualmente
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_IP", dotenv.get("DB_IP"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		// Si estamos en local, podemos cargar el archivo .env manualmente
+		String dbUsername = dotenv.get("DB_USERNAME");
+		String dbPassword = dotenv.get("DB_PASSWORD");
+		String dbIp = dotenv.get("DB_IP");
+		String dbName = dotenv.get("DB_NAME");
 
-		System.setProperty("MAIL_USERNAME", dotenv.get("MAIL_USERNAME"));
-		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
+		String mailUsername = dotenv.get("MAIL_USERNAME");
+		String mailPassword = dotenv.get("MAIL_PASSWORD");
+
+		// Si estamos en Docker, las variables de entorno ya estarán disponibles
+		// y no será necesario hacer nada adicional
+
+		// Solo usaremos System.setProperty si las variables no están en el entorno de Docker
+		if (dbUsername != null) {
+			System.setProperty("DB_USERNAME", dbUsername);
+		}
+		if (dbPassword != null) {
+			System.setProperty("DB_PASSWORD", dbPassword);
+		}
+		if (dbIp != null) {
+			System.setProperty("DB_IP", dbIp);
+		}
+		if (dbName != null) {
+			System.setProperty("DB_NAME", dbName);
+		}
+		if (mailUsername != null) {
+			System.setProperty("MAIL_USERNAME", mailUsername);
+		}
+		if (mailPassword != null) {
+			System.setProperty("MAIL_PASSWORD", mailPassword);
+		}
 
 		SpringApplication.run(DeepDiveRecordApplication.class, args);
 	}
-
 }
